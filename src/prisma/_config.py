d56a4c024b88653e133eb4097307e784be48352d
Michael Kang <michael3dk@gmail.com>
2025-09-15 17:40:47 -0700

    release: 0.16.3 with prisma 6.16.1
    Updates:
    - Upgrade Prisma CLI from 6.12.0 to 6.16.1
    - Upgrade Prisma Engine from 8047c96 to 1c57fdc (latest from main branch)
    - Bump version to 0.16.3

--- src/prisma/_config.py
+++ src/prisma/_config.py
@@ -27,13 +27,13 @@ class DefaultConfig(BaseSettings):
     #       doesn't change then the CLI is incorrectly cached
     prisma_version: str = Field(
         env='PRISMA_VERSION',
-        default='6.12.0',
+        default='6.16.1',
     )
 
     # Engine binary versions can be found under https://github.com/prisma/prisma-engine/commits/main
     expected_engine_version: str = Field(
         env='PRISMA_EXPECTED_ENGINE_VERSION',
-        default='8047c96bbd92db98a2abc7c9323ce77c02c89dbc',
+        default='1c57fdcd7e44b29b9313256c76699e91c3ac3c43',
     )
 
     # Home directory, used to build the `binary_cache_dir` option by default, useful in multi-user
